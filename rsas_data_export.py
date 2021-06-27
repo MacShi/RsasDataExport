e43c6204da36385ee608b9342f5e2d1f7886f14d
your name <you@example.com>
2021-06-27 15:12:02 +0800

    增加处理进度

--- rsas_data_export.py
+++ rsas_data_export.py
@@ -1,6 +1,7 @@
 
 from lxml import etree
 import pandas as pd
+import argparse
 
 def score2risk(score:float)->str:
     '''
@@ -19,16 +20,17 @@ def score2risk(score:float)->str:
         return "低"
 def parseXml(path:str):
     '''
-
     :param path: xml文件路径
     :return: 无返回
+    解析xml文件，用IP地址作为文件名称创建EXcel中，并将漏洞名称、威胁类型、CVE、风险等级、解决方法、漏洞描述写入Excel文件中
     '''
     parser = etree.XMLParser(encoding="utf-8")
     xml = etree.parse(path,parser=parser)
     targets = xml.xpath("//targets/target")
+    targets_count = len(targets)
+    sum = 0
     for target in targets:
         host = target.xpath("ip/text()")[0] if len(target.xpath("ip/text()")) ==1 else "无IP地址"
-        print(host)
         vulnXml = target.xpath("vuln_detail/vuln")
         vulInfos = []
         for vuln in vulnXml:
@@ -37,21 +39,19 @@ def parseXml(path:str):
             threat_category = vuln.xpath("threat_category/text()")[0] if len(vuln.xpath("threat_category/text()")) ==1 else "无"
             cve_id = vuln.xpath("cve_id/text()")[0] if len(vuln.xpath("cve_id/text()")) ==1 else "无"
             risk = score2risk(float(vuln.xpath("risk_points/text()")[0])) if len(vuln.xpath("risk_points/text()")) ==1 else "无"
-            # print(vuln.xpath("risk_points/text()")[0])
             solution = vuln.xpath("solution/text()")[0] if len(vuln.xpath("solution/text()")[0]) >0  else "无"
-            description =  vuln.xpath("description/text()")[0] if len(vuln.xpath("description/text()"))==1 else "无"
+            description =  vuln.xpath("description/text()")[0] if len(vuln.xpath("description/text()")) > 0 else "无"
             vulInfo.append(name)
             vulInfo.append(threat_category)
             vulInfo.append(cve_id)
             vulInfo.append(risk)
             vulInfo.append(solution)
             vulInfo.append(description)
-            # print("名字：{},威胁信息：{},CVE-ID：{},威胁等级：{},解决方法：{},描述:{}".format(name,threat_category,cve_id,risk,solution,description))
             vulInfos.append(vulInfo)
-    print(vulInfos)
-    df = pd.DataFrame(vulInfos, columns=['漏洞名称',"威胁类型","CVE","风险等级","解决方法", '漏洞描述'])
-    df.to_excel("{}.xlsx".format(host), index=False)
-
+        df = pd.DataFrame(vulInfos, columns=['漏洞名称',"威胁类型","CVE","风险等级","解决方法", '漏洞描述'])
+        df.to_excel("{}.xlsx".format(host), index=False)
+        sum = sum+1
+        print("处理进度 {}/{} ，主机{}处理完成".format(sum,targets_count,host))
 
 if __name__ == '__main__':
     parseXml(r'..\data\xml\1248.xml')
